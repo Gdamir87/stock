feb258663f7794e03281745ed8165fae39b556f5
Damir Gaifulin <gdamir@inbox.ru>
2024-02-24 12:23:47 +0300

    перевел запросы на JPA, без Hibernate

--- src/main/java/ru/damir/stock/controller/dto/ProductDAO.java
+++ src/main/java/ru/damir/stock/controller/dto/ProductDAO.java
@@ -1,8 +1,9 @@
 package ru.damir.stock.controller.dto;
 
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.damir.stock.entity.Product;
@@ -16,28 +17,38 @@ public class ProductDAO {
     private EntityManager entityManager;
 
     public List<Product> getAllProducts() {
-        Session session = entityManager.unwrap(Session.class);
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Query<Product> query = session.createQuery("from Product", Product.class);
+//        List<Product> allProducts = query.getResultList();
 
-        Query<Product> query = session.createQuery("from Product", Product.class);
+        Query query = entityManager.createQuery("from Product");
         List<Product> allProducts = query.getResultList();
         return allProducts;
-
     }
 
     public void saveProduct(Product product) {
-        Session session = entityManager.unwrap(Session.class);
-        session.persist(product);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.persist(product);
+        Product newProduct = entityManager.merge(product);
+        product.setId(newProduct.getId());
     }
 
     public Product getProduct(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Product product = session.get(Product.class, id);
+//        Session session = entityManager.unwrap(Session.class);
+//        Product product = session.get(Product.class, id);
+
+        Product product = entityManager.find(Product.class, id);
         return product;
     }
 
     public void deleteProduct(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Product> query = session.createQuery("delete from Product " +
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Product> query = session.createQuery("delete from Product " +
+//                "where id =: productId");
+//        query.setParameter("productId", id);
+//        query.executeUpdate();
+        Query query = entityManager.createQuery("delete from Product " +
                 "where id =: productId");
         query.setParameter("productId", id);
         query.executeUpdate();
